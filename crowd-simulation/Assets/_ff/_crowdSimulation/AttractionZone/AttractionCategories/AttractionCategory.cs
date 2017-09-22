d33d8d375cc0d88b09d97fc237bd1c67793d4f70
gdominikff <dominik.ganghofer@framefield.com>
2017-09-22 18:03:21 +0200

    Added Attraction Categories

---
+++ crowd-simulation/Assets/_ff/_crowdSimulation/AttractionZone/AttractionCategories/AttractionCategory.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "new AttractionCategory", menuName = "CrowdSimulation/AttractionCategory", order = 2)]
+public class AttractionCategory : ScriptableObject
+{
+    public Color Color = Color.white;
+}
+
