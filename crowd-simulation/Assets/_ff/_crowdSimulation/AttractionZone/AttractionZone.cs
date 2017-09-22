d0c57e6ee8dc1102a6b5f2356822a4f0a34de0b6
gdominikff <dominik.ganghofer@framefield.com>
2017-09-22 16:31:17 +0200

    Added AttractionZone

---
+++ crowd-simulation/Assets/_ff/_crowdSimulation/AttractionZone/AttractionZone.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AttractionZone : MonoBehaviour
+{
+    [SerializeField] AttractionCategory Category;
+    [SerializeField] float Radius;
+    [SerializeField] float AttractionStrength;
+    [SerializeField] AnimationCurve AttractionDistribution;
+
+    public int NumberOfCircles;
+
+    public enum AttractionCategory
+    {
+        Food,
+        Wine,
+        Textiles,
+        Pottery,
+        Philosophy
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnDrawGizmos()
+    {
+
+        for (int i = 0; i < NumberOfCircles; i++)
+        {
+            var radiusFraction = Radius * (i + 1) / NumberOfCircles;
+            // var thickness = AttractionDistribution.Evaluate((i + 1) / nCircles);
+            // AttractionDistribution.
+            Gizmos.color = new Color(1, 1, 1, AttractionDistribution.Evaluate((i + 1f) / NumberOfCircles));
+            var positions = GetCirclePositions(radiusFraction);
+            ConnectPositions(positions);
+        }
+
+    }
+
+    private Vector3[] GetCirclePositions(float radius)
+    {
+        var resolution = 32;
+        List<Vector3> allPositions = new List<Vector3>();
+        var pos = new Vector3(radius, 0, 0);
+
+        for (int i = 0; i < resolution; i++)
+        {
+            var circlePosition = Quaternion.Euler(0, i * 360 / resolution, 0) * pos;
+            allPositions.Add(transform.position + circlePosition);
+
+        }
+
+        return allPositions.ToArray();
+    }
+
+    private void ConnectPositions(Vector3[] positions)
+    {
+        for (int i = 0; i < positions.Length; i++)
+            Gizmos.DrawLine(positions[i], positions[(i + 1) % positions.Length]);
+    }
+
+    public float GetAttractivenessAtGlobalPosition(Vector3 spectator)
+    {
+        var distance = Vector3.Distance(spectator, this.transform.position);
+        var distanceNormalized = distance / Radius;
+        var attractiveness = AttractionDistribution.Evaluate(distanceNormalized);
+        return attractiveness;
+    }
+
+
+}
