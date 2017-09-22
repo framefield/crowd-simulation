6422de7604b7714760e6585a0f17d54a132098a5
gdominikff <dominik.ganghofer@framefield.com>
2017-09-22 18:03:53 +0200

    Added Agent Categories

---
+++ crowd-simulation/Assets/_ff/_crowdSimulation/Agent/AgentCategories/AgentCategory.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "new AgentCategory", menuName = "CrowdSimulation/AgentCategory", order = 1)]
+public class AgentCategory : ScriptableObject
+{
+    public Color AgentColor = Color.white;
+    public List<AttractionInterest> Interests;
+
+    //todo: use dictionary with custom inspector 
+    public float GetAttractionInterest(AttractionCategory attractionCategory)
+    {
+        foreach (var interest in Interests)
+            if (interest.AttractionCategory == attractionCategory)
+                return interest.InterestValue;
+        return 0f;
+    }
+
+    [Serializable]
+    public class AttractionInterest
+    {
+        public AttractionCategory AttractionCategory;
+        public float InterestValue;
+
+        public AttractionInterest(AttractionCategory attraction)
+        {
+            AttractionCategory = attraction;
+        }
+    }
+
+}
+
