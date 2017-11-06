976af4b890a1ba6287962529dd2e161bc06ea810
gdominikff <dominik.ganghofer@framefield.com>
2017-11-06 12:48:34 +0100

    Added SerializableDictionary

---
+++ crowd-simulation/Assets/SerializableDictionary/Example/Editor/UserSerializableDictionaryPropertyDrawers.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(StringStringDictionary))]
+[CustomPropertyDrawer(typeof(ObjectColorDictionary))]
+public class AnySerializableDictionaryPropertyDrawer : SerializableDictionaryPropertyDrawer {}
