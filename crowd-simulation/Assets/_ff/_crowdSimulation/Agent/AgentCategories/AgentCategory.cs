23dd32ccfd4d08587bda9f223ef13dd021f21556
gdominikff <dominik.ganghofer@framefield.com>
2017-12-05 14:37:38 +0100

    AgentCategory derives from InterestCategory to be treated equally in social interaction

--- crowd-simulation/Assets/_ff/_crowdSimulation/Agent/AgentCategories/AgentCategory.cs
+++ crowd-simulation/Assets/_ff/_crowdSimulation/Agent/AgentCategories/AgentCategory.cs
@@ -5,12 +5,11 @@ using System.Collections;
 using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "new AgentCategory", menuName = "CrowdSimulation/AgentCategory", order = 1)]
-public class AgentCategory : ScriptableObject
+public class AgentCategory : InterestCategory
 {
-    public Color AgentColor = Color.white;
     public float MaxTimeOnMarket = float.PositiveInfinity;
     public Interests Interests;
-    public SocialInterests SocialInterests;
+    public Interests SocialInterests;
     public float Stamina;
 }
 
