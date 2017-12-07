eddd34d7db651109d971feb7ada9fbc93a73d00f
gdominikff <dominik.ganghofer@framefield.com>
2017-12-07 14:30:25 +0100

    Added Range to floats in SerializableDictionary

--- crowd-simulation/Assets/SerializableDictionary/SerializableDictionary.cs
+++ crowd-simulation/Assets/SerializableDictionary/SerializableDictionary.cs
@@ -5,61 +5,62 @@ using UnityEngine;
 
 public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
 {
-	[SerializeField]
-	TKey[] m_keys;
-	[SerializeField]
-	TValue[] m_values;
+    [SerializeField]
+    TKey[] m_keys;
+    [SerializeField]
+    [Range(0f, 1f)]
+    TValue[] m_values;
 
-	public SerializableDictionary()
-	{
-	}
+    public SerializableDictionary()
+    {
+    }
 
-	public SerializableDictionary(IDictionary<TKey, TValue> dict) : base(dict.Count)
-	{
-		foreach (var kvp in dict)
-		{
-			this[kvp.Key] = kvp.Value;
-		}
-	}
+    public SerializableDictionary(IDictionary<TKey, TValue> dict) : base(dict.Count)
+    {
+        foreach (var kvp in dict)
+        {
+            this[kvp.Key] = kvp.Value;
+        }
+    }
 
-	public void CopyFrom(IDictionary<TKey, TValue> dict)
-	{
-		this.Clear();
-		foreach (var kvp in dict)
-		{
-			this[kvp.Key] = kvp.Value;
-		}
-	}
+    public void CopyFrom(IDictionary<TKey, TValue> dict)
+    {
+        this.Clear();
+        foreach (var kvp in dict)
+        {
+            this[kvp.Key] = kvp.Value;
+        }
+    }
 
-	public void OnAfterDeserialize()
-	{
-		if(m_keys != null && m_values != null && m_keys.Length == m_values.Length)
-		{
-			this.Clear();
-			int n = m_keys.Length;
-			for(int i = 0; i < n; ++i)
-			{
-				this[m_keys[i]] = m_values[i];
-			}
+    public void OnAfterDeserialize()
+    {
+        if (m_keys != null && m_values != null && m_keys.Length == m_values.Length)
+        {
+            this.Clear();
+            int n = m_keys.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                this[m_keys[i]] = m_values[i];
+            }
 
-			m_keys = null;
-			m_values = null;
-		}
+            m_keys = null;
+            m_values = null;
+        }
 
-	}
+    }
 
-	public void OnBeforeSerialize()
-	{
-		int n = this.Count;
-		m_keys = new TKey[n];
-		m_values = new TValue[n];
+    public void OnBeforeSerialize()
+    {
+        int n = this.Count;
+        m_keys = new TKey[n];
+        m_values = new TValue[n];
 
-		int i = 0;
-		foreach(var kvp in this)
-		{
-			m_keys[i] = kvp.Key;
-			m_values[i] = kvp.Value;
-			++i;
-		}
-	}
+        int i = 0;
+        foreach (var kvp in this)
+        {
+            m_keys[i] = kvp.Key;
+            m_values[i] = kvp.Value;
+            ++i;
+        }
+    }
 }
